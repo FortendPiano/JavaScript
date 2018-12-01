c09bcb6541935cdc4b4680ce85f0ffdcca2cec79
Andrey <andrey_y4in_1297@bk.ru>
2018-12-01 16:44:40 +0400

    Первый коммит

--- Lesson 9/87cba2ce086a78a21099eb4d202d6371/js/script.js
+++ Lesson 9/87cba2ce086a78a21099eb4d202d6371/js/script.js
@@ -92,26 +92,20 @@ window.addEventListener('DOMContentLoaded', function () {
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close'),
-        btn = document.querySelectorAll('.description-btn');
-
-    for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener('click', function () {
+        btn = document.querySelectorAll('.description-btn'),
+        func = function () {
             overlay.style.display = 'block';
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
-        });
+        };
+    more.addEventListener('click', func);
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', func);
     }
-    more.addEventListener('click', function () {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
-    });
-
     close.addEventListener('click', function () {
         overlay.style.display = 'none';
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
-
 
 });
